411f170ef939c3d2456bc4877f715d02974ff6ee
Marina <144914691+Mariin5@users.noreply.github.com>
2023-11-10 14:50:32 +0900

    Add files via upload

---
+++ kadai_013/kadai_013.py
@@ -0,0 +1,21 @@
+import random
+
+def calculate_total(price):
+    includetax_price=price*1.1
+    return price
+price=random.randint(100,1000)
+print(price)
+
+
+#以下は提出一回目の内容
+#import random
+
+#price = random.randint(100,1000)
+#print (f"税抜き{price}円")
+
+#def calculate_total(price,tax):
+    #total=price+tax
+    #print(f"税込み{total}円")
+
+
+
